8fbf20d14fe28fc7ca2844138134ecbcf1c3ddbe
miradonis <miradonis@web.de>
2023-02-05 17:30:24 +0100

    added bookingBox

---
+++ src/components/bookingForm/BookingForm.jsx
@@ -0,0 +1,33 @@
+import './BookingForm.css';
+import ChooseDate from '../../assets/img/icon/bookingImg.svg';
+import GreenArrow from '../../assets/img/icon/greenArrow.png';
+import GreenArrowNew from '../../assets/img/icon/arrowGreen.png';
+import BookingPerson from '../../assets/img/icon/bookingPerson.svg';
+
+const BookingForm = () => {
+
+    return (
+        <section className='bookingFormSection'>
+            {
+                /* ----------Choose Date---------- */
+            }
+            <div className='bookingFormChooseDate'>
+                <img className='bookingFormImg' src={ChooseDate} />
+                <p className='bookingFormParagraph'>Arrival</p>
+                <img className='bookingFormGreenArrow' src={GreenArrowNew} />
+                <img className='bookingFormImg' src={ChooseDate} />
+                <p className='bookingFormParagraph'>Departure</p>
+            </div>
+            {
+                /* -----Choose Room and number of people----- */
+            }
+            <div className='bookingChoosePersonRoom'>
+                <img className='bookingPersonImg' src={BookingPerson} />
+                <p className='bookingPersonParagraph'>1 Room, 1 Adult</p>
+            </div>
+            <button className='bookingBtn'>Check</button>
+        </section>
+    );
+}
+
+export default BookingForm;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Certifications from '../../components/certifications/Certifications';
 import IconsAndText from '../../components/iconsAndText/IconsAndText';
 import Rooms from '../../components/rooms/Rooms';
 import Footer from '../../components/footer/Footer';
+import BookingForm from '../../components/bookingForm/BookingForm';
 
 const Home = () => {
     {/* big picture as background ------ components inserted */ }
@@ -10,12 +11,14 @@ const Home = () => {
     return (
         <section className='homeSection'>
             {/* <article className='homeArticle'></article> */}
+            {/* <BookingForm /> */}
             <article className='firstSection' >
-                <div className='firstBox'>
+                {/* <div className='firstBox'>
                     <p className='firstText'>
                         Experience a stay of exceptional class at  <span className='firstSpan'>Hotel Lamoure</span> - where excellence becomes a habit
                     </p>
-                </div>
+                </div> */}
+                <BookingForm />
             </article>
 
             {/* -------------- COMPONENTS ---------------- */}
